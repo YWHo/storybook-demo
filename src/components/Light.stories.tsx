5f289d4b8f7e8f8bd36a2a85a66f8daba33d6b7d
Brian Ho <YWHo@users.noreply.github.com>
2024-02-18 13:56:45 +1300

    add custom render function to Light stories

--- src/components/Light.stories.tsx
+++ src/components/Light.stories.tsx
@@ -24,3 +24,31 @@ export const Red: Story = {
     variant: "red",
   },
 };
+
+export const Another: Story = {
+  args: {
+    variant: "red",
+  },
+  // custom render
+  render: (args) => <Light {...args} />,
+};
+
+export const Grouped: Story = {
+  // custom render to see the effect of compose
+  render: () => (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 10,
+        border: "1px solid black",
+        width: "max-content",
+        padding: 10,
+      }}
+    >
+      <Light variant="red" />
+      <Light variant="yellow" />
+      <Light variant="green" />
+    </div>
+  ),
+};
